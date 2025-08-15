d609562c78b03b9c4b2339c52ac9cc75f640c184
neq <neq17320251@gmail.com>
2025-08-15 23:38:58 +0800

    文件整理

---
+++ leetcode/LeetCode100/哈希/1-两数之和.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum1 = function (nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; ++i) {
+        if (map.has(target - nums[i])) {
+            return [i, map.get(target - nums[i])];
+        }
+        map.set(nums[i], i);
+    }
+}
+var twoSum = function (nums, target) {
+    let obj = [];
+
+    // put in obj
+    for (let i = 0; i < nums.length; ++i) {
+        obj.push({ val: nums[i], idx: i });
+    }
+
+    // sort by val
+    obj = obj.sort((a, b) => a.val - b.val);
+
+    // two pointers
+    let l = 0 , r = obj.length - 1;
+    while (l < r) {
+        if(obj[l].val + obj[r].val === target) {
+            return [obj[l].idx, obj[r].idx];
+        }
+        else if(obj[l].val + obj[r].val < target) {
+            l++;
+        }
+        else {
+            r--;
+        }
+    }
+
+
+};
+
+let nums = [3 , 2 ,4];
+let target = 6;
+let result = twoSum(nums, target);
+console.log(result);  // 输出：[0, 1]
